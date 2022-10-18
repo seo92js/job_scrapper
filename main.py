a065cc411aa15fdad9dc413da553987b5d758ea9
Seo JeongSeop <seo92js@gmail.com>
2022-10-19 01:03:22 +0900

    file export 기능 추가

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
 from extractors.saramin import extract_saramin_jobs
+from file import save_to_file
 
 app = Flask("JobScrapper")
+
+db = {}
 
 @app.route("/")
 def home():
     return render_template("index.html", name="nico")
 
 @app.route("/search")
-def hello():
+def search():
     keyword = request.args.get("keyword")
-    indeed = extract_indeed_jobs(keyword)
-    wwr = extract_wwr_jobs(keyword)
-    saramin = extract_saramin_jobs(keyword)
-    jobs =  indeed + wwr + saramin
+    if keyword == None or keyword == "":
+        return redirect("/")
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        indeed = extract_indeed_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+        saramin = extract_saramin_jobs(keyword)
+        jobs =  indeed + wwr + saramin
+        db[keyword] = jobs
     return render_template("search.html", keyword = keyword, jobs = jobs)
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None or keyword == "":
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
     
 app.run("127.0.0.1")
